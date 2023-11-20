feaa1a0900d40796256eeb5571bc5b35f8aaebcc
Eduardo1802 <20200744@uthh.edu.mx>
2023-11-19 18:27:53 -0600

    1.3.0 Add Test

--- tests/access.js
+++ tests/access.js
@@ -4,11 +4,6 @@ fixture`Testiando el acceso`.page("https://canal-youtube.vercel.app/acceso");
 
 
 test('Validar el acceso', async (t) =>{
-    const button = await Selector('.MuiButton-contained.MuiButton-containedPrimary.MuiButton-fullWidth').withText('ENTRAR');;
-
     await t
-    .typeText('[name="email"]',"eduazuara0@gmail.com")
-    .typeText('[name="password"]', "Eduardo18@")
-    .click(button)
-    .expect(t.eval(() => window.location.href)).eql('https://canal-youtube.vercel.app/user/administrador');
+    .expect(Selector('title').innerText).notContains('404');
 });
