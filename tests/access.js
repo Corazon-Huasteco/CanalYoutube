e9fc972b70a734234057a2a575ece94042078712
Eduardo1802 <20200744@uthh.edu.mx>
2023-11-19 18:19:59 -0600

    1.2.0 Add Test

--- tests/access.js
+++ tests/access.js
@@ -1,11 +1,10 @@
 const { Selector } = require("testcafe");
-
+                                   
 fixture`Testiando el acceso`.page("https://canal-youtube.vercel.app/acceso");
 
 
 test('Validar el acceso', async (t) =>{
     const button = await Selector('.MuiButton-contained.MuiButton-containedPrimary.MuiButton-fullWidth').withText('ENTRAR');;
-    const title = await Selector('.MuiTypography-root.MuiTypography-h5.MuiTypography-gutterBottom.css-adrpgk').withText('Hola, Bienvenido');
 
     await t
     .typeText('[name="email"]',"eduazuara0@gmail.com")
