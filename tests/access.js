73a8a8499cfa697395a65ce65e53a0b08094c4bb
Eduardo1802 <20200744@uthh.edu.mx>
2023-11-19 18:48:56 -0600

    1.3.0 Add Test

--- tests/access.js
+++ tests/access.js
@@ -1,10 +1,21 @@
+// const { Selector } = require("testcafe");
+
+// fixture`Testiando el acceso`.page("https://canal-youtube.vercel.app/acceso");
+
+// test('Validar el acceso', async (t) => {
+//     const pageTitle = await Selector('title').innerText;
+//     const is404 = pageTitle.includes('404');
+
+//     await t
+//         .expect(is404).notOk('Page should not be a 404 error');
+// });
+
 const { Selector } = require("testcafe");
 
 fixture`Testiando el acceso`.page("https://canal-youtube.vercel.app/acceso");
 
 test('Validar el acceso', async (t) => {
-    const pageTitle = await Selector('title').innerText;
-    const is404 = pageTitle.includes('404');
+    const is404 = await Selector('.error-title').exists;
 
     await t
         .expect(is404).notOk('Page should not be a 404 error');
