1ff89ce14f031891449df3d2c1cfc80ca22fafba
Eduardo1802 <20200744@uthh.edu.mx>
2023-11-19 18:41:38 -0600

    1.3.0 Add Test

--- tests/access.js
+++ tests/access.js
@@ -2,7 +2,6 @@ const { Selector } = require("testcafe");
                                    
 fixture`Testiando el acceso`.page("https://canal-youtube.vercel.app/acceso");
 
-
 test('Validar el acceso', async (t) =>{
     await t
     .expect(Selector('title').innerText).notContains('404');
